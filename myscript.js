a746cfd3589de41aeafa96c8bd1335173ff58911
kasem32833 <abulkasem32833@gmail.com>
2021-01-20 14:42:39 +0300

    Thsi is programming Practice
    This is a Frontend design for CSE student semester fee calculator

---
+++ myscript.js
@@ -0,0 +1,17 @@
+<script>
+    per_creditt = 3000;
+
+    function total_credit(x, y){
+
+        totalFees= (x+Y)*per_credit;
+        return totalFees;
+    }
+
+
+
+
+    promt("Please pay your tution fees:");
+
+
+
+</script>
